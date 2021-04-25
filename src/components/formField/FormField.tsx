7ec188115c5c43646a76e2c2822f36bfeee405cc
Grzegorz <cymborski@gmail.com>
2021-04-25 12:42:24 +0200

    formfieldprops type fix

--- src/components/formField/FormField.tsx
+++ src/components/formField/FormField.tsx
@@ -11,7 +11,7 @@ type FormFieldProps = {
   errorName?: string;
   isValid: boolean;
   isInvalid: boolean;
-  as?: "input";
+  as?: React.ElementType;
   children?: ReactNode;
   errorVisible: boolean;
   type?: string;
